3c0214d1a6a03c9169717177e2a0a518b6c9920f
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-02-20 23:58:04 -0500

    changed app file name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App.js';
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
